926c9c899c7e36974c0e3c75c07ae0e702518dd9
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-07-14 15:21:04 -0600

    Make favorite recipes display when favorite recipes button is clicked and make recipes to cook display when recipes to cook button is clicked #44 #45

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,12 @@
 //query selectors
 const body = document.querySelector("body");
-const recipeCard = document.querySelector(".main-page");
+const iconButtonBackground = document.querySelector("div");
+const favoriteRecipesButton = document.querySelector(".favorite-recipes-button");
+const homeButton = document.querySelector(".home")
+const recipesToCookButton = document.querySelector(".recipes-to-cook-button");
+const favoriteRecipesPage = document.querySelector(".favorite-recipes");
+const recipesToCookPage = document.querySelector(".recipes-to-cook");
+const mainPage = document.querySelector(".main-page");
 const recipeImage = document.querySelector(".recipe-image");
 const username = document.querySelector(".username");
 
@@ -22,9 +28,16 @@ function loadPage() {
 function clickHandler() {
 	if (event.target.classList.contains("heart")) {
 		determineFavoriteRecipe();
+    // changeButtonColor();
 	} else if (event.target.classList.contains("frying-pan")) {
 		determineRecipeToCook();
-	}
+  } else if (event.target.classList.contains("home")) {
+    displayMainPage();
+	} else if (event.target.classList.contains("favorite-recipes-button")) {
+    displayFavoriteRecipes();
+  } else if (event.target.classList.contains("recipes-to-cook-button")) {
+    displayRecipesToCook();
+  }
 };
 
 //other functions
@@ -64,16 +77,74 @@ function displayUsername() {
 };
 
 function displayAllRecipes() {
-	recipeCard.innerHTML = ``;
+	mainPage.innerHTML = ``;
   cookbook.allRecipes.forEach(recipe => {
-    recipeCard.innerHTML += `
+    mainPage.innerHTML += `
     <article class="recipe-card">
       <img class="recipe-image" src="${recipe.image}" alt="${recipe.name}">
       <section class="recipe-graphics">
-        <div>
+        <div class="">
           <input type="image" src="../assets/heart.png" class="icon heart ${recipe.id}">
         </div>
-        <div>
+        <div class="">
+          <input type="image" src="../assets/frying-pan.png" class="icon frying-pan ${recipe.id}">
+        </div>
+      </section>
+      <h4>${recipe.name}</h4>
+    </article>
+    `;
+  });
+};
+
+function changeButtonColor() {
+  // console.log(iconButtonBackground)
+  // iconButtonBackground.classList.add("clicked");
+  // console.log(iconButtonBackground.classList)
+};
+
+function displayMainPage() {
+  mainPage.classList.remove("hidden");
+  favoriteRecipesPage.classList.add("hidden");
+  recipesToCookPage.classList.add("hidden");
+};
+
+function displayFavoriteRecipes() {
+  mainPage.classList.add("hidden");
+  recipesToCookPage.classList.add("hidden");
+  favoriteRecipesPage.classList.remove("hidden");
+  favoriteRecipesPage.innerHTML = ``;
+  user.favoriteRecipes.forEach(recipe => {
+    favoriteRecipesPage.innerHTML += `
+    <article class="recipe-card">
+      <img class="recipe-image" src="${recipe.image}" alt="${recipe.name}">
+      <section class="recipe-graphics">
+        <div class="">
+          <input type="image" src="../assets/heart.png" class="icon heart ${recipe.id}">
+        </div>
+        <div class="">
+          <input type="image" src="../assets/frying-pan.png" class="icon frying-pan ${recipe.id}">
+        </div>
+      </section>
+      <h4>${recipe.name}</h4>
+    </article>
+    `;
+  });
+};
+
+function displayRecipesToCook() {
+  mainPage.classList.add("hidden");
+  favoriteRecipesPage.classList.add("hidden");
+  recipesToCookPage.classList.remove("hidden");
+  recipesToCookPage.innerHTML = ``;
+  user.recipesToCook.forEach(recipe => {
+    recipesToCookPage.innerHTML += `
+    <article class="recipe-card">
+      <img class="recipe-image" src="${recipe.image}" alt="${recipe.name}">
+      <section class="recipe-graphics">
+        <div class="">
+          <input type="image" src="../assets/heart.png" class="icon heart ${recipe.id}">
+        </div>
+        <div class="">
           <input type="image" src="../assets/frying-pan.png" class="icon frying-pan ${recipe.id}">
         </div>
       </section>
